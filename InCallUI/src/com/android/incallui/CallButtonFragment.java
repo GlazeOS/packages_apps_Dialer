900f8834b8c61403ed45a2a827e633f92ae2599d
Qiang Chen <cqiang@codeaurora.org>
2016-06-23 17:54:06 +0800

    nCallUI: Add support for call recording
    - Add support for call recording
    
    (cherry-picked from Id06ce3fa994e2bf70992c46e0f73a6fed6c9460a)
    (cherry-picked from I0dc949dbee5247086247ebf3e9bc2ed871e8f7f5)
    
    CRs-Fixed: 934290
    
    Change-Id: Ia9774bb1b5f22baf2da49f3f6383058ffbeea4ae

--- InCallUI/src/com/android/incallui/CallButtonFragment.java
+++ InCallUI/src/com/android/incallui/CallButtonFragment.java
@@ -32,6 +32,7 @@ import static com.android.incallui.CallButtonFragment.Buttons.BUTTON_TRANSFER_AS
 import static com.android.incallui.CallButtonFragment.Buttons.BUTTON_TRANSFER_BLIND;
 import static com.android.incallui.CallButtonFragment.Buttons.BUTTON_TRANSFER_CONSULTATIVE;
 import static com.android.incallui.CallButtonFragment.Buttons.BUTTON_UPGRADE_TO_VIDEO;
+import static com.android.incallui.CallButtonFragment.Buttons.BUTTON_RECORD;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
@@ -95,7 +96,8 @@ public class CallButtonFragment
         public static final int BUTTON_TRANSFER_BLIND = 12;
         public static final int BUTTON_TRANSFER_ASSURED = 13;
         public static final int BUTTON_TRANSFER_CONSULTATIVE = 14;
-        public static final int BUTTON_COUNT = 15;
+        public static final int BUTTON_RECORD = 15;
+        public static final int BUTTON_COUNT = 16;
     }
 
     private SparseIntArray mButtonVisibilityMap = new SparseIntArray(BUTTON_COUNT);
@@ -117,6 +119,7 @@ public class CallButtonFragment
     private ImageButton mAssuredTransferButton;
     private ImageButton mConsultativeTransferButton;
     private ImageButton mAddParticipantButton;
+    private ImageButton mRecordButton;
 
     private PopupMenu mAudioModePopup;
     private boolean mAudioModePopupVisible;
@@ -193,6 +196,8 @@ public class CallButtonFragment
         mManageVideoCallConferenceButton = (ImageButton) parent.findViewById(
                 R.id.manageVideoCallConferenceButton);
         mManageVideoCallConferenceButton.setOnClickListener(this);
+        mRecordButton = (ImageButton) parent.findViewById(R.id.recordButton);
+        mRecordButton.setOnClickListener(this);
         return parent;
     }
 
@@ -254,10 +259,19 @@ public class CallButtonFragment
             getPresenter().callTransferClicked(QtiImsExtUtils.QTI_IMS_CONSULTATIVE_TRANSFER);
         } else if (id == R.id.overflowButton) {
             if (mOverflowPopup != null) {
+                updateRecordMenu();
                 mOverflowPopup.show();
             }
         } else if (id == R.id.manageVideoCallConferenceButton) {
             onManageVideoCallConferenceClicked();
+        } else if (id == R.id.recordButton) {
+            if (!((InCallActivity) getActivity()).isCallRecording()) {
+                ((InCallActivity) getActivity()).startInCallRecorder();
+                mRecordButton.setBackgroundResource(R.drawable.btn_stop_record);
+            } else {
+                ((InCallActivity) getActivity()).stopInCallRecorder();
+                mRecordButton.setBackgroundResource(R.drawable.btn_start_record);
+            }
         } else {
             Log.wtf(this, "onClick: unexpected");
             return;
@@ -266,6 +280,14 @@ public class CallButtonFragment
         view.performHapticFeedback(
                 HapticFeedbackConstants.VIRTUAL_KEY,
                 HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING);
+    }
+
+    private void updateRecordMenu() {
+        MenuItem item = mOverflowPopup.getMenu().findItem(BUTTON_RECORD);
+        if (item != null) {
+            item.setTitle(((InCallActivity) getActivity()).isCallRecording() ?
+                    R.string.menu_stop_record : R.string.menu_start_record);
+        }
     }
 
     public void updateColors() {
@@ -399,6 +421,7 @@ public class CallButtonFragment
         mOverflowButton.setEnabled(isEnabled);
         mManageVideoCallConferenceButton.setEnabled(isEnabled);
         mAddParticipantButton.setEnabled(isEnabled);
+        mRecordButton.setEnabled(isEnabled);
     }
 
     @Override
@@ -445,6 +468,8 @@ public class CallButtonFragment
             return mAssuredTransferButton;
         } else if (id == BUTTON_TRANSFER_CONSULTATIVE) {
             return mConsultativeTransferButton;
+        } else if (id == BUTTON_RECORD) {
+            return mRecordButton;
         } else {
             Log.w(this, "Invalid button id");
             return null;
